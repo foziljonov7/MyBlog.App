3cf1437b82271220864f007be01acf85fe89b156
foziljonov7 <abdulvosidfoziljonov55@gmail.com>
2023-10-27 14:25:19 +0500

    Add project files.

---
+++ MyBlog.Application/UseCases/Post/Commands/CreatePostCommand.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.Application.UseCases.Post.Commands
+{
+    public class CreatePostCommand : IRequest
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+
+    public class CreatePostCommandHandler : AsyncRequestHandler<CreatePostCommand>
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        protected override Task Handle(CreatePostCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
